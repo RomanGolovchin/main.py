01ded391710136c7eb19208b4dea45b8423f884f
Роман <istina82new@gmail.com>
2024-11-21 01:06:31 +0300

    21.11.2024

---
+++ main.py
@@ -0,0 +1,28 @@
+           #Здесь пишется то, что необходимо вывести
+        #Определяет тип данных в скобках
+
+# print(type('Hello World!'))   #int - сокращение от intemger - целое число #float - дробное число #str - сокращённо от string - строка
+# print(type(True), type(False)) #bool - сокращение от Boolean - логический тип данных
+# print(type(int('10')))
+# print(9**0,5*5)
+# print(9.99 > 9.98 and 1000 != 1000.1)
+# print(2*2+2)
+# print(2*(2+2))
+# print(2*2+2==2*(2+2))
+# print(int(float(123.456)*10)%10)
+
+print(12+89-1)
+"!st program"
+print(9**0.5*5)
+"2nd program"
+print(9.99>9.98 and 1000 !=1000.1)
+"3rd program"
+print(2*2+2)
+print(2*(2+2))
+print(2*2+2 == 2*(2+2))
+"4th program"
+"123.456"
+print((float(123.456)*10))
+print(int(float(123.456)*10)%10)
+print(type(float(123.456)))
+
